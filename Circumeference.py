e73c8216c2dc979c19c79a7de398c5a5d3dc4b4a
Tani-alt <Tanirika.gupta@gmail.com>
2025-02-21 19:03:54 +0530

    calculation

---
+++ Circumeference.py
@@ -0,0 +1,12 @@
+import math
+def calculate_circumference(radius):
+    return 2*math.pi*radius
+radius=float(input("enter the number of radius"))
+circumference=calculate_circumference(radius)
+print(f"The circumference of the circle with radius {radius} is: {circumference}")
+
+
+
+
+ 
+
